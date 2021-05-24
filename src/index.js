7d0c4b338d38e390ea1f2a9ccc6f8b0f35c37ebb
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-25 06:37:12 +1000

    fetch working, for loop on returned data working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+function fetchDog(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(`${imgUrl}`)
+    .then(response => response.json())
+    .then(json =>
+        dawgs(json))
+}
+
+function dawgs(dogs){
+    let array = dogs.message
+    console.log(array)
+    for(i=0;i<array.length;i++){
+        let each = array[i]
+        console.log(each)
+    }
+
+}
+
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function(){
+    fetchDog()
+})
