ebe9ac6921bd04dba682c690f918e1e45fae3867
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-25 21:42:25 +1000

    trying to work out event listener for drop down selection

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function dawgs(dogs){
     let array = dogs.message
     let dogImages = document.getElementById('dog-image-container')
     console.log(array)
-    array.forEach(i => {each = i
+    array.filter(i => {each = i
         console.log(each)
         let img = dogImages.appendChild(document.createElement('img'))
         Object.assign(img,{
@@ -30,15 +30,41 @@ function fetchBreeds(){
 
 function breedsah(breeds){
     let dogBreeds = breeds.message
-    Object.keys(dogBreeds).forEach(dog => {
-        console.log(dog)
+    Object.keys(dogBreeds).filter(dog => {
+        let dogBreedList = document.getElementById('dog-breeds')
+        let listItem = dogBreedList.appendChild(document.createElement('li'))
+        listItem.innerHTML = dog
+        listItem.onclick = function dogClick(){
+            listItem.style.color = 'red'
+        }
     })
 }
 
+function fetchBreedsForDropdown(){
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(json =>
+        dropDownBreeds(json))
+}
 
+function dropDownBreeds(breeds){
+    let dogBreeds = breeds.message
+    let startsWithA = Object.keys(dogBreeds).filter(dog => dog[0] === 'A' || dog[0] === 'a')
+    let startsWithB = Object.keys(dogBreeds).filter(dog => dog[0] === 'B' || dog[0] === 'b')
+    let startsWithC = Object.keys(dogBreeds).filter(dog => dog[0] === 'C' || dog[0] === 'c')
+    let startsWithD = Object.keys(dogBreeds).filter(dog => dog[0] === 'D' || dog[0] === 'd')
+    let dropDown = document.getElementById('breed-dropdown')
+    let dogBreedList = document.getElementById('dog-breeds')
+    dropDown.addEventListener('change', function(){
+        if (dropDown.value === 'a'){return breedsah(startsWithA)
+    }
+})
+}
 
 console.log('%c HI', 'color: firebrick')
 document.addEventListener('DOMContentLoaded', function(){
     fetchDog()
     fetchBreeds()
+    fetchBreedsForDropdown()
 })
